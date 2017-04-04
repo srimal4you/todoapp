7d30b302efc4456a5b3f2d61cf8075c5b53f7704
HMSNishantha <srimal@3cs.lk>
2017-04-04 09:53:02 +0530

    BugFix : Fixed a error which was exist on imports section, user module

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '../../services/authentication.service';
+import { AuthenticationService } from '../../services/authentication/authentication.service';
 import { User } from '../../classes/user';
 
 @Component({
