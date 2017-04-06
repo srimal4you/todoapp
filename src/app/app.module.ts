846f95b36a2dd87ac3051f3735c2bf6504b2df51
HMSNishantha <srimal@3cs.lk>
2017-04-06 18:05:27 +0530

    Added dependencies for google material UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './modules/router.module';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
@@ -10,6 +11,7 @@ import { UserComponent } from './components/user/user.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthenticationService } from './services/authentication/authentication.service';
 import { AuthGuard } from './guards/auth.guard';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,7 @@ import { AuthGuard } from './guards/auth.guard';
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [AuthGuard, AuthenticationService],
