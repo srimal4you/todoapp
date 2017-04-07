3c61cd60b482767af5517c3059ec21bf2ef8fa1d
H.M.S.Nishantha <srimal@3cs.lk>
2017-04-08 00:24:58 +0530

    Updated template files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-// import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-  // providers: [Router]
 })
 
 export class AppComponent {
